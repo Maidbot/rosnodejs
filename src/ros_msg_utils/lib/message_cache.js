80850b1c7f6f4a63b9e23dd5bbf23b437f12c428
Nick Sweet <nckswt@gmail.com>
2019-09-08 19:44:39 -0500

    Allow environment variables using React.

--- src/ros_msg_utils/lib/message_cache.js
+++ src/ros_msg_utils/lib/message_cache.js
@@ -20,7 +20,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const CMAKE_PREFIX_PATH = process.env.CMAKE_PREFIX_PATH;
+// See https://create-react-app.dev/docs/adding-custom-environment-variables
+// to understand how to use process.env in React.
+const CMAKE_PREFIX_PATH = process.env.CMAKE_PREFIX_PATH || process.env.REACT_APP_CMAKE_PREFIX_PATH;
 if (!CMAKE_PREFIX_PATH) {
   throw new Error('Unable to find CMAKE_PREFIX_PATH environment variable. Did you source setup.bash?')
 }
